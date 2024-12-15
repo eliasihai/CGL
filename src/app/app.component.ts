8e3e14a08bf2469f487261bdbbe73fc10e527acf
eliasi hai <eliasihai@hotmail.com>
2024-12-15 02:31:59 +0200

    upload the solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+// Components
+import { RegisterComponent } from './components/register/register.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, RegisterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'CGL';
