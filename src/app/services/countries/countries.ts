d3f966a47f166dac71496e116c89d838014deb8e
eliasi hai <eliasihai@hotmail.com>
2024-12-15 20:37:20 +0200

    code completed

--- src/app/services/countries/countries.ts
+++ src/app/services/countries/countries.ts
@@ -1,17 +1,7 @@
-export class Country implements ICountry {
-  public id: number;
-  public countryName: string;
-  public phoneCode: string;
-
-  constructor(
-    public Id: number,
-    public CountryName: string,
-    public PhoneCode: string
-  ) {
-    this.id = Id;
-    this.countryName = CountryName;
-    this.phoneCode = PhoneCode;
-  }
+export interface Country{
+  id: number;
+  countryName: string;
+  phoneCode: string;
 }
 
 export interface ICountry {
