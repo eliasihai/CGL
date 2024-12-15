c8fec5bdaa6ce389f41b491d6da39766e9765818
eliasi hai <eliasihai@hotmail.com>
2024-12-15 20:35:36 +0200

    code completed

--- src/app/services/countries/counties.service.ts
+++ src/app/services/countries/counties.service.ts
@@ -13,12 +13,12 @@ export class CountiesService {
   constructor(private httpClient: HttpClient) { }
 
   getCountries(): Observable<Country[]> {
-    return this.httpClient.get<any>(this.url, { headers: { Accept: 'application/json' } }).pipe(
+    return this.httpClient.get<any>(this.url).pipe(
       map((response) => {
         try {
           const parsedResponse = JSON.parse(response);
           return (
-            parsedResponse?.map((country: Country) => ({
+            parsedResponse?.map((country: ICountry) => ({
               id: country?.Id,
               countryName: country?.CountryName,
               phoneCode: country?.PhoneCode
